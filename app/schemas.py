5310f2254b3a50e912239d15b7ae48d904789575
woodstock93 <woodstock93@naver.com>
2024-05-13 11:42:33 +0900

    🍎Chore: 모델 설정

---
+++ app/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class URLBase(BaseModel):
+    original_url: str
+
+class URL(URLBase):
+    is_active: bool
+    clicks: int
+    
+    class Config:
+        orm_mode = True
+
+class URLInfo(URL):
+    short_url: str
+    admin_url: str
